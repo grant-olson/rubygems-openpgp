a70472f22327ab72c464928aabaec9c054c061c2
Grant Olson <kgo@grant-olson.net>
2011-05-28 22:49:05 -0400

    Disable vinstall and sbuild totally

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -2,5 +2,5 @@ require 'rubygems/command_manager'
 
 Gem::CommandManager.instance.register_command :sign
 Gem::CommandManager.instance.register_command :verify
-Gem::CommandManager.instance.register_command :vinstall
-Gem::CommandManager.instance.register_command :sbuild
+#Gem::CommandManager.instance.register_command :vinstall
+#Gem::CommandManager.instance.register_command :sbuild
