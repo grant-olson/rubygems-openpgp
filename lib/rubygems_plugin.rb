607dc71ae544c7e9ff3a3387cddaa387534d9453
Yorick Peterse <yorickpeterse@gmail.com>
2013-02-05 21:46:31 +0100

    Verify Gems upon installation.
    Signed-off-by: Yorick Peterse <yorickpeterse@gmail.com>

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,6 +1,9 @@
 require 'rubygems/command_manager'
+require 'rubygems/gem_openpgp'
 
 Gem::CommandManager.instance.register_command :sign
 Gem::CommandManager.instance.register_command :verify
-#Gem::CommandManager.instance.register_command :vinstall
-#Gem::CommandManager.instance.register_command :sbuild
+
+Gem.pre_install do |installer|
+  installer.say(Gem::OpenPGP.verify_gem(installer.gem))
+end
