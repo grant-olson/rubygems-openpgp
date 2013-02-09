6829a3ca332c6c11bdef6f1316f9c048093c3180
Grant Olson <kgo@grant-olson.net>
2013-02-09 17:53:16 -0500

    --sign option on gem build #10

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -4,6 +4,33 @@ require 'rubygems/gem_openpgp'
 Gem::CommandManager.instance.register_command :sign
 Gem::CommandManager.instance.register_command :verify
 
+# gem build hooks
+b = Gem::CommandManager.instance[:build]
+b.add_option("--sign", "Sign gem with OpenPGP.") do |value, options|
+  Gem::OpenPGP.options[:sign] = true
+end
+
+b.add_option('--key KEY', "Specify key id if you don't want to use your default gpg key") do |key, options|
+  Gem::OpenPGP.options[:key] = key
+end
+
+class Gem::Commands::BuildCommand
+  alias_method :original_execute, :execute
+  def execute
+    original_execute
+
+    if Gem::OpenPGP.options[:sign]
+      gemspec = get_one_gem_name
+      if File.exist? gemspec then
+        spec = Gem::Specification.load gemspec
+        file_name = File.join(".", File.basename(spec.cache_file))
+        Gem::OpenPGP.sign_gem file_name, key=Gem::OpenPGP.options[:key]
+      end
+    end
+  end
+end
+
+# gem install hooks
 i = Gem::CommandManager.instance[:install]
 i.add_option("--verify",
              'Verifies a local gem that has been signed via OpenPGP.' +
